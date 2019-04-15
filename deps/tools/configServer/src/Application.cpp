9f12dde55d3a8cd6306f9ce9531de39b6a1fc79c
David Fiel <github@dfiel.com>
2019-04-15 14:14:03 -0400

    Typo...

--- deps/tools/configServer/src/Application.cpp
+++ deps/tools/configServer/src/Application.cpp
@@ -25,7 +25,7 @@ std::shared_ptr<Application> Application::GetInstance() {
   return inst;
 }
 
-void Application::Set(wpi::StringRef appType, wstd::string gstEnv,
+void Application::Set(wpi::StringRef appType, std::string gstEnv,
                       std::function<void(wpi::StringRef)> onFail) {
   wpi::StringRef appDir;
   wpi::StringRef appEnv;
